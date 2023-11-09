b83163311a3cd0cc44349485faf123c907da373a
Vinicius de Castro Coutinho <viniciuscoutinho_@hotmail.com>
2023-11-09 18:49:19 -0300

    commit33

--- MaguilaBus/main.js
+++ MaguilaBus/main.js
@@ -1,9 +1,21 @@
+const precoPassagem = 140;
 let  valorTotalCompra = 0;
 let assentosSelecionados = [];
 
 
 function selecionarCadeira(cadeira) {
     const cadeiraSelecionada = cadeira;
+    if(cadeiraSelecionada.classList.contains("selecionado")){
+        cadeiraSelecionada.classList.remove("selecionado");
+        assentosSelecionados = assentosSelecionados.filter(id => id !== cadeiraSelecionada.id);
+        valorTotalCompra = assentosSelecionados.length * precoPassagem;
+        return;
+    }
     cadeiraSelecionada.classList.add("selecionado")
     assentosSelecionados.push(cadeiraSelecionada.id);
+    valorTotalCompra = assentosSelecionados.length * precoPassagem;
+}
+
+function atualizarVisualizacaoPreco(){
+    
 }
